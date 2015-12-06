b8bd5b9007f851fa25035ba83679d5547991a5b8
srli <sophia.li@students.olin.edu>
2015-12-06 16:27:36 -0500

    added motor integration

--- masterArduino.ino
+++ masterArduino.ino
@@ -1,5 +1,18 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include <Stepper.h>
+
+//MOTOR CONSTANTs:
+const int mSpeed = 60;
+const int stepsPerRevolution = 100;  // change this to fit the number of steps per revolution
+const int mSpeed1 = 3000;
+const int mSpeed2 = 3000;
+const int stepsPerRevolution1 = 5;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution2 = 5;  // change this to fit the number of steps per revolution
+
+// initialize the stepper library on pins 8 through 11:
+Stepper M1(stepsPerRevolution1, 22, 23, 24, 25);
+Stepper M2(stepsPerRevolution2, 26, 27, 28, 29);
 
 // set up LEDs
 int light_mode = 0;
@@ -16,6 +29,9 @@ long prevTime = 0;
 byte mode = 0;
 
 void setup() {
+  M1.setSpeed(mSpeed1);
+  M2.setSpeed(mSpeed2);
+
   pinMode(red_pin, OUTPUT);
   pinMode(blue_pin, OUTPUT);
   pinMode(green_pin, OUTPUT);
@@ -32,16 +48,16 @@ void loop() {
     case 1:
       serialFlush();
       resetMotorLED();
-      wave();
-      serialFlush();
-      resetMotorLED();
-      mode = 0;
+      M1.step(stepsPerRevolution1);
+      M2.step(stepsPerRevolution2);
+      //mode = 0;
       break;
     default:
       if ((millis() - prevTime) > 1000){
         brightness = map(analogRead(photoDiode_pin), 500, 900, 0, 255);
         prevTime = millis();
       }
+      Serial.println("default");
       analogWrite(red_pin, brightness);
 //      analogWrite(blue_pin, brightness);
 //      analogWrite(green_pin, brightness);
--- n_stepper/n_stepper.ino
+++ n_stepper/n_stepper.ino
@@ -1,12 +1,18 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 500;  // change this to fit the number of steps per revolution
 const int mSpeed = 60;
+const int stepsPerRevolution = 100;  // change this to fit the number of steps per revolution
+const int mSpeed1 = 3000;
+const int mSpeed2 = 3000;
+const int stepsPerRevolution1 = 5;  // change this to fit the number of steps per revolution
+const int stepsPerRevolution2 = 5;  // change this to fit the number of steps per revolution
+
 // for your motor
 
 // initialize the stepper library on pins 8 through 11:
-Stepper M1(stepsPerRevolution, 22, 23, 24, 25);
-Stepper M2(stepsPerRevolution, 26, 27, 28, 29);
+Stepper M1(stepsPerRevolution1, 22, 23, 24, 25);
+//Stepper M1(stepsPerRevolution1, 4,5,6,7);
+Stepper M2(stepsPerRevolution2, 26, 27, 28, 29);
 Stepper M3(stepsPerRevolution, 30, 31, 32, 33);
 Stepper M4(stepsPerRevolution, 34, 35, 36, 37);
 Stepper M5(stepsPerRevolution, 38, 39, 40, 41);
@@ -14,10 +20,12 @@ Stepper M6(stepsPerRevolution, 42, 43, 44, 45);
 Stepper M7(stepsPerRevolution, 46, 47, 48, 49);
 Stepper M8(stepsPerRevolution, 50, 51, 52, 53);
 
+//int count;
+
 void setup() {
   // set the speed at 60 rpm:
-  M1.setSpeed(mSpeed);
-  M2.setSpeed(mSpeed);
+  M1.setSpeed(mSpeed1);
+  M2.setSpeed(mSpeed2);
   M3.setSpeed(mSpeed);
   M4.setSpeed(mSpeed);
   M5.setSpeed(mSpeed);
@@ -25,19 +33,21 @@ void setup() {
   M7.setSpeed(mSpeed);
   M8.setSpeed(mSpeed);
   // initialize the serial port:
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
   // step one revolution  in one direction:
-  Serial.println("clockwise");
-  M1.step(stepsPerRevolution);
-  M2.step(stepsPerRevolution);
-  delay(500);
-
+//  Serial.println("clockwise");
+  M1.step(stepsPerRevolution1);
+  M2.step(stepsPerRevolution2);
   // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  M1.step(-stepsPerRevolution);
-  M2.step(-stepsPerRevolution);
-  delay(500);
+//  Serial.println("counterclockwise");
+//  count = 0;
+//  while (count < 500){
+//    count ++;
+//    M1.step(-stepsPerRevolution);
+//    M2.step(-stepsPerRevolution);
+//  }
+//  delay(500);
 }
